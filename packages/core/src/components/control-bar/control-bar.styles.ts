5fff35969c004e7018bc35f577edb957edbc01d8
COCO SHAYAN <shayanzamiri.class@gmail.com>
2025-10-06 01:12:40 +0330

    Fix(control-bar):left controlbar options list's transparency's problem fixed.

--- packages/core/src/components/control-bar/control-bar.styles.ts
+++ packages/core/src/components/control-bar/control-bar.styles.ts
@@ -465,6 +465,7 @@ ease;;
     -webkit-appearance: none;
     -moz-appearance: none;
     background: url('data:image/svg+xml;utf8,%3Csvg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"%3E%3Cpath d="M6 9L12 15L18 9" stroke="%23000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/%3E%3C/svg%3E');
+    background-color: #fff; 
     background-repeat: no-repeat;
     background-position: right 4px center;
     width: max-content;
@@ -476,6 +477,7 @@ ease;;
     display: flex;
     align-items: center;
     justify-content: center;
+    background-color: #fff;
   }
  
 
