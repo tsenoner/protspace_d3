007376524c85603e986378619dc3db6fe180d84e
Tobias O <tobias.olenyi@tum.de>
2025-10-14 19:29:23 +1300

    docs: fix text in multiple projections
    Signed-off-by: Tobias O <tobias.olenyi@tum.de>

--- packages/core/src/components/scatter-plot/scatter-plot.stories.ts
+++ packages/core/src/components/scatter-plot/scatter-plot.stories.ts
@@ -138,11 +138,10 @@ export const MultipleProjections: Story = {
     <div style="display: grid; grid-template-columns: 1fr 1fr 1fr; gap: 1rem;">
       ${args.data.projections.map(
         (proj, idx) => html`
-          <div>
-            <h3 style="margin: 0 0 0.5rem 0; text-align: center;">
-              ${proj.name}
-            </h3>
-            <div style="width: 100%; height: 400px; border: 1px solid #ccc;">
+            <div style="width: 100%; height: 100%; border: 1px solid #ccc; position: relative;">
+              <h3 style="text-align: center; position: relative; z-index: 1;">
+                ${proj.name}
+              </h3>
               <protspace-scatterplot
                 .data=${args.data}
                 .selectedProjectionIndex=${idx}
@@ -150,7 +149,7 @@ export const MultipleProjections: Story = {
                 .useCanvas=${true}
               ></protspace-scatterplot>
             </div>
-          </div>
+
         `,
       )}
     </div>
